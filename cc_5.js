b561666715470a1d50f14e0eb74d7e36afca6def
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-19 12:17:54 -0400

    Create Manager subclass with bonus calculation.

--- cc_5.js
+++ cc_5.js
@@ -18,3 +18,20 @@ class Employee {
         return this.salary * 12     // Multiply monthly salary by 12 and return the result
     }
 }
+
+// Create a subclass Manager
+class Manager extends Employee {
+    constructor (name, salary, department) { 
+        super(name, salary);            // Inherit "name" and "salary" properties from class Employee class
+        this.department = department;   // Initiate the instance property "department"
+    }
+
+    annualSalary () {
+        // This method overrides the annualSalary method in the parrent class        
+        let annual = this.salary * 12  // Multiply the monthly salary by 12
+        let bonus = annual * 0.15;      // Calculate 15% bonus
+        
+        // Display the amount of bonus and total salary 
+        console.log(`The amouth of the bonus is $${bonus}, and the total annual salary is $${annual + bonus}.`);
+    }
+}
