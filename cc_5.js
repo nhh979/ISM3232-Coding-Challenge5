d6f171b854e61984a5bc33023ef33b198ca0e626
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-19 11:30:35 -0400

    Implement Employee class with name, salary properties, and annual salary calculation.

---
+++ cc_5.js
@@ -0,0 +1,14 @@
+// Hoang Nguyen
+// U12840485
+
+class Employee {
+    constructor (name, salary) {
+        // Create the instances of the Employee class with 'name' and 'salary' properties
+        this.name = name;
+        this.salary = salary;
+
+        // Display employee's name and salary
+        console.log(`Employee name: ${this.name}.`);
+        console.log(`Monthly salary: $${this.salary}.`);
+    }
+}
