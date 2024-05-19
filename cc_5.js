4c8aae1b4d0a640a1823c37285a6c247030c36d2
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-19 11:43:00 -0400

    Update the Employee class

--- cc_5.js
+++ cc_5.js
@@ -1,6 +1,7 @@
 // Hoang Nguyen
 // U12840485
 
+// Create Employee class
 class Employee {
     constructor (name, salary) {
         // Create the instances of the Employee class with 'name' and 'salary' properties
@@ -10,5 +11,10 @@ class Employee {
         // Display employee's name and salary
         console.log(`Employee name: ${this.name}.`);
         console.log(`Monthly salary: $${this.salary}.`);
+    }
+
+    annualSalary () {
+        // This function is used to calculate the annual salary
+        return this.salary * 12     // Multiply monthly salary by 12 and return the result
     }
 }
